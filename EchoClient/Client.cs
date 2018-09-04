26aaa1393ebb8f7949b263c55f84b0c1228edcb5
AndersSkovNielsen <anderssnielsen@live.dk>
2018-09-04 12:56:20 +0200

    Tilføjet Client

---
+++ EchoClient/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EchoClient
+{
+    class Client
+    {
+        public Client()
+        {
+            
+        }
+
+        public void Start()
+        {
+            Stream ns;
+
+            using (TcpClient socket = new TcpClient("localhost", 7))
+
+                ns = socket.GetStream();
+
+            using (StreamReader sr = new StreamReader(ns))
+            using (StreamWriter sw = new StreamWriter(ns))
+
+            {
+                sw.WriteLine("Anders");
+                sw.Flush();
+
+                String line = sr.ReadLine();
+
+                Console.WriteLine(line);
+            } // using slut = close forbindelse til socket / client
+        }
+    }
+}
---
+++ EchoClient2/Client.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EchoClient2
+{
+    class Client
+    {
+        public Client()
+        {
+
+        }
+
+        public void Start()
+        {
+            using (TcpClient socket = new TcpClient("localhost", 77))
+                
+            using (NetworkStream ns = socket.GetStream())
+
+            using (StreamReader sr = new StreamReader(ns))
+            using (StreamWriter sw = new StreamWriter(ns))
+
+            {
+                sw.WriteLine("Anders");
+                sw.Flush();
+
+                String line = sr.ReadLine();
+
+                Console.WriteLine(line);
+            } // using slut = close forbindelse til socket / client
+        }
+    }
+}
