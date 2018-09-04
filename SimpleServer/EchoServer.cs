587d304167290139b611a926e0a6bc0b50c60824
AndersSkovNielsen <anderssnielsen@live.dk>
2018-09-04 11:58:50 +0200

    Add project files.

---
+++ SimpleServer/EchoServer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SimpleServer
+{
+    internal class EchoServer
+    {
+        private readonly int pORT;
+
+        public EchoServer(int pORT)
+        {
+            this.pORT = pORT;
+        }
+
+        internal void Start()
+        {
+            TcpListener serverListener = new TcpListener(IPAddress.Loopback, pORT);
+            serverListener.Start();
+
+            using (TcpClient socket = serverListener.AcceptTcpClient())
+            using (StreamReader sr = new StreamReader(socket.GetStream()))
+            using (StreamWriter sw = new StreamWriter(socket.GetStream()))
+
+            {
+                String incomingString = sr.ReadLine();
+
+                Console.WriteLine($"String in = {incomingString}");
+
+                sw.WriteLine(incomingString);
+                sw.Flush();
+
+            } // using slut = close forbindelse til socket / client
+        }
+    }
+}
---
+++ SimpleServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SimpleServer
+{
+    class Program
+    {
+        private const int PORT = 7;
+        static void Main(string[] args)
+        {
+            EchoServer server = new EchoServer(PORT);
+            server.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
